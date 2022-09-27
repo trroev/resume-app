6f280aa6e0ccb682605842bb1abb1b5b4ef40656
Trevor Mathiak <tmathiak@gmail.com>
2022-09-27 06:20:47 -0400

    completed education component

--- src/components/Resume/Form/Education.js
+++ src/components/Resume/Form/Education.js
@@ -1,67 +1,24 @@
 import React from "react";
+import EducationItem from "./EducationItem";
 
-const Education = ({ educationInfo }) => {
+const Education = ({ id, educationInfo, onChange, onAdd, onRemove }) => {
+  const educationItems = educationInfo.map((educationItem) => (
+    <EducationItem
+      key={educationItem.id}
+      id={educationItem.id}
+      educationItem={educationItem}
+      onChange={onChange}
+      onAdd={onAdd}
+      onRemove={onRemove}
+    />
+  ));
   return (
     <div className="form-container">
       <div className="form-header">
         <h2>Education</h2>
       </div>
-      <form className="form">
-        <div className="form-control">
-          <label>School</label>
-          <input
-            type="text"
-            name="school"
-            placeholder="Michigan State University"
-            value={educationInfo.school}
-          />
-        </div>
-        <div className="form-control">
-          <label>Location</label>
-          <input
-            type="text"
-            name="location"
-            placeholder="East Lansing, MI"
-            value={educationInfo.location}
-          />
-        </div>
-        <div className="form-control">
-          <label>Degree</label>
-          <input
-            type="text"
-            name="degree"
-            placeholder="Bachelor's"
-            value={educationInfo.degree}
-          />
-        </div>
-        <div className="form-control">
-          <label>Major</label>
-          <input
-            type="text"
-            name="degree"
-            placeholder="Computer Science"
-            value={educationInfo.major}
-          />
-        </div>
-        <div className="form-control">
-          <label>Start Date</label>
-          <input
-            type="text"
-            name="startDate"
-            placeholder="2005"
-            value={educationInfo.startDate}
-          />
-        </div>
-        <div className="form-control">
-          <label>End Date</label>
-          <input
-            type="text"
-            name="endDate"
-            placeholder="2009"
-            value={educationInfo.endDate}
-          />
-        </div>
-      </form>
+      {educationItems}
+      <button onClick={() => onAdd(id)}>Add</button>
     </div>
   );
 };
---
+++ src/components/Resume/Form/EducationItem.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+const EducationItem = ({ id, educationItem, onChange, onRemove }) => {
+  return (
+    <>
+      <form className="form">
+        <div className="form-control">
+          <label>School</label>
+          <input
+            type="text"
+            name="school"
+            placeholder="Michigan State University"
+            value={educationItem.school}
+            onChange={(e) => onChange(e, id)}
+          />
+        </div>
+        <div className="form-control">
+          <label>Location</label>
+          <input
+            type="text"
+            name="location"
+            placeholder="East Lansing, MI"
+            value={educationItem.location}
+            onChange={(e) => onChange(e, id)}
+          />
+        </div>
+        <div className="form-control">
+          <label>Degree</label>
+          <input
+            type="text"
+            name="degree"
+            placeholder="Bachelor's"
+            value={educationItem.degree}
+            onChange={(e) => onChange(e, id)}
+          />
+        </div>
+        <div className="form-control">
+          <label>Major</label>
+          <input
+            type="text"
+            name="degree"
+            placeholder="Computer Science"
+            value={educationItem.major}
+            onChange={(e) => onChange(e, id)}
+          />
+        </div>
+        <div className="form-control">
+          <label>Start Date</label>
+          <input
+            type="text"
+            name="startDate"
+            placeholder="2005"
+            value={educationItem.startDate}
+            onChange={(e) => onChange(e, id)}
+          />
+        </div>
+        <div className="form-control">
+          <label>End Date</label>
+          <input
+            type="text"
+            name="endDate"
+            placeholder="2009"
+            value={educationItem.endDate}
+            onChange={(e) => onChange(e, id)}
+          />
+        </div>
+        <button onClick={() => onRemove(id)}>Remove</button>
+      </form>
+    </>
+  );
+};
+
+export default EducationItem;
